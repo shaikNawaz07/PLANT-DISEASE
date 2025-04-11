723c37900562fea33a1d92d6d4fad9f925722483
Nawaz <97388511+shaikNawaz07@users.noreply.github.com>
2025-04-11 23:00:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 #Sidebar
-st.sidebar.title("AgriSens")
+st.sidebar.title("FarmCure")
 app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
 #app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
 
