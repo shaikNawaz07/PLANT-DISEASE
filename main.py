e46e97fcbfd037547bd029039adafa94e15eb7e1
Nawaz <97388511+shaikNawaz07@users.noreply.github.com>
2025-04-18 19:46:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Load an image to display at the top of the title
-top_image = "top_image.jpg"  # Replace with the path to your image file
+top_image = "Diseases.png"  # Replace with the path to your image file
 
 # Apply CSS styles for centering elements
 center_style = """
